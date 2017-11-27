a8b0cf52a55a1d44f6cd560ca933bdd68b4d29c4
Raj Patel <raj.z.patel@rbc.com>
2017-11-27 16:35:00 -0500

    Create, Read, Update and Delete Heroes using an in memory Web API that acts as a Server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,6 +24,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  deleteHero(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
